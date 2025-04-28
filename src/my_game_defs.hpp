a4e3c5235a4331049995a2da9eee53c7e6cf3fae
Neil McGill <goblinhack@gmail.com>
2025-04-28 19:06:24 +0100

    fewer tiles visible

--- src/my_game_defs.hpp
+++ src/my_game_defs.hpp
@@ -16,7 +16,7 @@
 #define MAP_SCROLL_BORDER    0.5
 #define MAP_SCROLL_SPEED     50
 #define TICK_DURATION_MS     50
-#define MAP_TILES_ACROSS_DEF 20
-#define MAP_TILES_DOWN_DEF   20
+#define MAP_TILES_ACROSS_DEF 16
+#define MAP_TILES_DOWN_DEF   16
 
 #endif
