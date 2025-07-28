9a58aafea3c671f418f18b40d2a444bfd0ed3c8e
Goblin Hack <goblinhack@gmail.com>
2025-07-28 17:34:04 +0100

    fix tar warnings

--- build/check_files.sh
+++ build/check_files.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
-# https://stackoverflow.com/questions/51655657/tar-ignoring-unknown-extended-header-keyword-libarchive-xattr-security-selinux
 untar() {
-  set -x
-    TAR_FLAGS="--no-same-owner --no-same-permissions --warning=no-unknown-keyword --warning=no-timestamp --delay-directory-restore"
-    tar -zxvf $TAR_FLAGS $*
-
-    TAR_FLAGS="--no-same-owner --no-same-permissions --warning=no-unknown-keyword "
-    tar -zxvf $TAR_FLAGS $*
-
-    TAR_FLAGS="--warning=no-unknown-keyword"
-    tar -zxvf $TAR_FLAGS $*
-  set +x
-    if [[ $? -eq 0 ]]; then
-        return
+    echo tar -zxvf $*
+    tar -zxvf $*
+    if [[ $? -ne 0 ]]; then
+        echo $0: Failed to extract archive $*
+        exit 1
     fi
-    tar zxvf $*
+}
+
+make_tar() {
+    # https://stackoverflow.com/questions/51655657/tar-ignoring-unknown-extended-header-keyword-libarchive-xattr-security-selinux
+    echo tar zcvf -no-xattrs $*
+    tar zcvf -no-xattrs $*
+    if [[ $? -ne 0 ]]; then
+        echo tar zcvf $*
+        tar zcvf $*
+        if [[ $? -ne 0 ]]; then
+            echo $0: Failed to create archive $*
+            exit 1
+        fi
+    fi
 }
 
 if [[ ! -d data/gfx ]]; then
     echo $0: Need to extract graphics archive
     untar data/gfx.tgz
-    if [[ $? -ne 0 ]];
-    then
-        echo $0: Failed to extract data/gfx.tgz
-        exit 1
-    fi
     DONE=1
 fi
 
 if [[ ! -d data/sounds ]]; then
     echo $0: Need to extract sounds archive
     untar data/sounds.tgz
-    if [[ $? -ne 0 ]];
-    then
-        echo $0: Failed to extract data/sounds.tgz
-        exit 1
-    fi
     DONE=1
 fi
 
@@ -53,50 +48,26 @@ COUNT=$(find data/gfx -newer data/gfx.tgz -type f | wc -l)
 if [[ $COUNT -gt 0 ]];
 then
     echo $0: Need to retar graphics tarball due to updates
-    (
-        tar zcvf data/gfx.tgz data/gfx
-        if [[ $? -ne 0 ]];
-        then
-            echo $0: Failed to zip data/gfx.tgz
-            exit 1
-        fi
-    )
+    make_tar data/gfx.tgz data/gfx
 fi
 
 COUNT=$(find data/sounds -newer data/sounds.tgz -type f | wc -l)
 if [[ $COUNT -gt 0 ]];
 then
     echo $0: Need to retar sounds tarball due to updates
-    (
-        tar zcvf data/sounds.tgz data/sounds
-        if [[ $? -ne 0 ]];
-        then
-            echo $0: Failed to zip data/sonuds.tgz
-            exit 1
-        fi
-    )
+    make_tar data/sounds.tgz data/sounds
 fi
 
 if [[ ! -d data/gfx ]];
 then
     echo $0: Unzip graphics
-    tar zxf data/gfx.tgz
-    if [[ $? -ne 0 ]];
-    then
-        echo $0: Failed to unzip data/gfx.tgz
-        exit 1
-    fi
+    untar data/gfx.tgz
 fi
 
 if [[ ! -d data/sounds ]];
 then
     echo $0: Unzip sounds
-    tar zxf data/sounds.tgz
-    if [[ $? -ne 0 ]];
-    then
-        echo $0: Failed to unzip data/sounds.tgz
-        exit 1
-    fi
+    untar data/sounds.tgz
 fi
 
 #
