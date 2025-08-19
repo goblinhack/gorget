e719f3dee28ba261ce6a9fee35623094881c2315
Goblin Hack <goblinhack@gmail.com>
2025-08-19 15:04:29 +0100

    fix build

---
+++ src/my_line.hpp
@@ -0,0 +1,16 @@
+//
+// Copyright goblinhack@gmail.com
+//
+
+#pragma once
+
+#ifndef _MY_LINE_HPP_
+#define _MY_LINE_HPP_
+
+#include "my_spoint.hpp"
+
+#include <vector>
+
+std::vector< spoint > draw_line(const spoint s, const spoint e, size_t max_elems);
+
+#endif
