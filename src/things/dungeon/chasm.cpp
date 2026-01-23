0504520fa7a69cda915d02da43ffa93aec8bebbf
Goblin Hack <goblinhack@gmail.com>
2026-01-23 10:07:06 +0000

    no teleporting onto chasms

--- src/things/dungeon/chasm.cpp
+++ src/things/dungeon/chasm.cpp
@@ -34,6 +34,7 @@ bool tp_load_chasm(void)
   tp_flag_set(tp, is_described_cursor);
   tp_flag_set(tp, is_needs_move_confirm);
   tp_flag_set(tp, is_obs_to_explosion);
+  tp_flag_set(tp, is_obs_to_teleporting_onto);
   tp_flag_set(tp, is_teleport_blocked);
   tp_flag_set(tp, is_tick_end_delay);
   tp_flag_set(tp, is_tickable);
