7aa70a7019c174898f92f3963c3b3253a4ded730
Goblin Hack <goblinhack@gmail.com>
2026-03-27 17:30:39 +0000

    tests ok now

--- build/build.sh
+++ build/build.sh
@@ -874,18 +874,10 @@ if [[ $OPT_GITHUB_BUILD != "" ]]; then
 
     LSAN_OPTIONS=max_leaks=10 ./${TARGET} --tests --debug
     if [[ $? -ne 0 ]]; then
-        cat ${TARGET}.stdout.txt
-        find appdata
-        cat appdata/${TARGET}/stdout.txt
         ls -la
-        find . -name stdout.txt
+        cat appdata/${TARGET}/stdout.txt
         exit 1
     fi
-        cat ${TARGET}.stdout.txt
-        find appdata
-        cat appdata/${TARGET}/stdout.txt
-        ls -la
-        find .
 fi
 
 exit 0
