22966387d51f66d8df280e73cbbc2a01c902ba1d
Goblin Hack <goblinhack@gmail.com>
2026-06-03 09:57:56 +0100

    repeat tests !

--- build/build.sh
+++ build/build.sh
@@ -872,7 +872,7 @@ if [[ $OPT_GITHUB_BUILD != "" ]]; then
       cp .o/${TARGET}.pdb .
     fi
 
-    LSAN_OPTIONS=max_leaks=10 ./${TARGET} --tests --debug
+    LSAN_OPTIONS=max_leaks=10 ./${TARGET} --tests --debug --repeat 10
     if [[ $? -ne 0 ]]; then
         if [[ "$APPDATA" != "" ]]; then
           ls -la $APPDATA
