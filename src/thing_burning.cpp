2c1a4b6ffb6b11b628a594e477ba3f8a74679ef7
Goblin Hack <goblinhack@gmail.com>
2026-06-06 14:58:35 +0100

    better message for burning

--- src/thing_burning.cpp
+++ src/thing_burning.cpp
@@ -84,7 +84,11 @@ void thing_continue_to_burn_check(Gamep g, Levelsp v, Levelp l, Thingp me)
         (void) thing_spawn(g, v, l, tp_first(is_fire), me);
 
         if (thing_is_player(me)) {
-          topcon(UI_IMPORTANT_FMT_STR "The flames intensify!" UI_RESET_FMT);
+          if (thing_is_immune_to(g, v, l, me, THING_EVENT_FIRE_DAMAGE)) {
+            topcon(UI_IMPORTANT_FMT_STR "The flames intensify, but you don't care!" UI_RESET_FMT);
+          } else {
+            topcon(UI_IMPORTANT_FMT_STR "The flames intensify!" UI_RESET_FMT);
+          }
         }
       }
     }
